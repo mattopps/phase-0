9d4f616ff7a73ecfcf1b2769b6db410732d8d03f
mattopps <mattopps@gmail.com>
2015-11-11 15:41:23 -0500

    add address file

---
+++ week4/address/my_solution.rb
@@ -0,0 +1,5 @@
+def make_address (street, city, state, zip)
+  "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+end
+
+make_address("123 state st.", "new city", "LA", 12345)  
