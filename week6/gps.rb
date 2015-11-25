2f5e96e06094f95db1f5203a8d0ce0552ee83ebe
mattopps <mattopps@gmail.com>
2015-11-25 01:09:47 -0500

    add nesting and gps

--- week6/gps.rb
+++ week6/gps.rb
@@ -1,42 +1,106 @@
 # Your Names
-# 1)
-# 2)
+# 1) Matthew Oppenheimer
+# 2) The guide. Celeen
 
-# We spent [#] hours on this challenge.
-
+# We spent [1] hours on this challenge. And then I refactored by myself. A bit over another hour.
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+# def serving_size_calc(order, order_quantity)# creates a method which accepts two arguments - a food order and how many people to prepare for.
+#    menu = {"cookie" => 1, "cake" =>  5, "pie" => 7} # this is a list of the available food that can be prepared. IE a Menu
+  
+# #   error_counter = 3 # I have no idea what this is right now.
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
+#   menu.each do |food|
+#     if menu[food] != menu[order]
+#       p error_counter += -1
+#     end
+#   end
+
+#   if error_counter > 0
+#     raise ArgumentError.new("#{order} is not a valid input")
+#   end
+
+#   serving_size = menu.values_at(order)[0]
+#   serving_size_mod = order_quantity % serving_size
+
+#   case serving_size_mod
+# #   when 0
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{order}"
+#   else
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{order}, you have #{serving_size_mod} leftover ingredients. Suggested baking order MAKE THIS FEATURE"
+#   end
+# end
+# #.each do |food, ##suggested_size##| iterating for just the value of a key/value pair in a hash requires two variables
+# #     p food
+# #     p "Library[food] is #{library[food]}"
+#     p "Library[item_to_make] #{library[item_to_make]}"
+# if library[food] != library[item_to_make] # determines if the food requested is in the list of foods that can be prepared. 
+     # p error_counter -= 1 # changes the error counter.
+   # end
+  #end
+
+#Refactor
+
+def serving_size_calc(order, party_size) 
+  menu = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+
+  unless menu.has_key?(order) 
+    raise ArgumentError.new("Sorry, we are out of #{order} right now.")
   end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  serving_size = menu[order]
+  extra_ingredients = party_size % serving_size
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
-
-  case serving_size_mod
+  case extra_ingredients
   when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-  else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Make #{party_size/serving_size} #{order}"
+  when 1..4
+    return "Make #{party_size/serving_size} #{order}, you have #{extra_ingredients} leftover ingredients. You could make #{extra_ingredients} cookies"
+  when 5
+    return "Make #{party_size/serving_size} #{order}, you have #{extra_ingredients} leftover ingredients. You could make #{extra_ingredients} cookies, or a cake"
+  when 6
+    return "Make #{party_size/serving_size} #{order}, you have #{extra_ingredients} leftover ingredients. You could make #{extra_ingredients} cookies, or a cake and a cookie"
   end
 end
 
-p serving_size_calc("pie", 7)
-p serving_size_calc("pie", 8)
-p serving_size_calc("cake", 5)
-p serving_size_calc("cake", 7)
-p serving_size_calc("cookie", 1)
-p serving_size_calc("cookie", 10)
-p serving_size_calc("THIS IS AN ERROR", 5)
+# p serving_size_calc("pie", 7)
+# p serving_size_calc("pie", 13)
+# p serving_size_calc("pie", 14)
+ #p serving_size_calc("pie", 153)
+
+#p serving_size_calc("cake", 4)
+#p serving_size_calc("cake", 9)
+
+#p serving_size_calc("cookie", 1)
+#p serving_size_calc("cookie", 10)
+#p serving_size_calc("croissant", 10)
+
+#p serving_size_calc("THIS IS AN ERROR", 5)
+
 
 #  Reflection
+=begin
+
+What did you learn about making code readable by working on this challenge?
+  -English is preferable to code.
+
+Did you learn any new methods? What did you learn about them?
+  I do not believe so.
+
+What did you learn about accessing data in hashes? 
+  
+  - it is very important to remember that keys and values have to be accessed differently and separately, otherwise the pair is accessed at once.
+
+What concepts were solidified when working through this challenge?
+ That counter iterations can be very roundabout and there is a more direct method possible. Grammar is tricky. I want to refactor to make the singular/plural more accurate. It is not correct to say 'make 21 pie', nor is it correct to say 'make 1 cookie' or '1 extra ingredients' But I have spent a lot of time on this already. 
+ That hashes are unique 
+ That variable names in loops shouldnt be confused with variable names or arguments from methods. I think I really do prefer x or some generic thing to go in the pipes ||. Having |food| unnecessarily threw me off. if it was |x| I don't think I would have been thrown off.
+ That variable names are more readable and make the code more easily followed if they are in normal english, and not as a command (item_to_make). food doesnt come in a library, but is usually referenced in a menu. What in the world is a serving_size_mod??? Things like that.
+
+=end
+
+
+
+
+
+
--- week6/nested-data-solution.rb
+++ week6/nested-data-solution.rb
@@ -1,3 +1,4 @@
+
 # RELEASE 2: NESTED STRUCTURE GOLF
 # Hole 1
 # Target element: "FORE"
@@ -6,7 +7,7 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
 # attempts:
 # ============================================================
-
+p array[1][1][2][0]
 
 
 # ============================================================
@@ -19,7 +20,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 # attempts:
 # ============================================================
 
-
+p hash[:outer][:inner]["almost"][3]
 
 # ============================================================
 
@@ -32,16 +33,65 @@ nested_data = {array: ["array", {hash: "finished"}]}
 # attempts:
 # ============================================================
 
-
-
-# ============================================================
-
+p nested_data[:array][1][:hash]
 # RELEASE 3: ITERATE OVER NESTED STRUCTURES
 
 number_array = [5, [10, 15], [20,25,30], 35]
 
+# # RELEASE 3: ITERATE OVER NESTED STRUCTURES
 
-
+original_array = [5, [10, 15], [20,25,30], 35]
+p new_array = original_array.dup.flatten.map {|number| number + 5}
+original_array.flatten!.map! {|number| number + 5}
+p new_array
+p original_array
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+
+#non-destructive
+
+startup_names.flatten.map {|name| name + 'ly, '}
+p startup_names
+p startup_names
+
+#destructive
+#new_names = startup_names.dup.flatten.map {|name| print name + 'ly, '}
+# or
+startup_names.flatten!.map! {|name| name + 'ly, '}
+#I have no idea why but with a print inside the code block the items are replaced in each case with nil. startup_names.flatten!.map! {|name| print name + 'ly, '}
+####
+# This was my 'refactor' Because I was wildly confused and didn't realize that I already had the solution refactored for both the destructive and nondestructive methods. the only difference is '!'
+
+# startup_names.each { 
+#     |x|
+#   if x.is_a? String
+#   	print x + "ly, "
+#   elsif x.kind_of?(Array)
+#     x.each {|x| if x.kind_of?(Array)
+#         x.each {|x| print x + 'ly, '}
+#   else
+#     print x + 'ly, '
+#   end
+# }
+# end}
+
+# This works, but it is so clunky, and I didn't really know what I was doing. I copied the code from the example in the releases on canvas and then copied it again a layer deeper. I just ran it 20 times and kept cutting and pasting and switching around the ifs, elses, and 'end' and '}' as the errors indicated. But it works.
+# Another drawback - it is pre-designed with the number of total distinct arrays in mind. It would not work on a further nested level. It is not a truly
+#I actually think just using x as a variable throughout is easier to read than inner, outer, whatever.
+
+#Reflection
+=begin
+
+What are some general rules you can apply to nested arrays?
+
+I don't get the meaning of this question.
+
+What are some ways you can iterate over nested arrays?
+you can flatten them and then work with just the one array. That is definitely the easiest thing to do. 
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+I used flatten for what I thought was my initial solution but was really the refactor. weird. Flatten is great. I was already familiar. It was my immediate first instinct. It removes the challenge of nested arrays because it is no longer nested.
+=end
